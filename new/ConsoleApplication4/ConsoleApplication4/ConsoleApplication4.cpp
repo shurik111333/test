fab35eae3b3e08b3f8ccdc3f983eac01de021b7f
Aleksandr Chudov <chudov.aleksandr94@gmail.com>
2016-03-19 19:30:13 +0300

    Trash

---
+++ new/ConsoleApplication4/ConsoleApplication4/ConsoleApplication4.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	if (sqrt(2) != sqrt(2))
+	{
+		cout << "Unequal" << endl;
+	}
+	else
+	{
+		cout << "Equal" << endl;
+	}
+	return 0;
+}
